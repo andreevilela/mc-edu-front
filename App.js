943e882cd8abd849cbd6d7f0a4f1445a62796514
andreevilela <andrevibegoogle@hotmail.com>
2021-11-01 00:48:36 -0300

    google signin

--- App.js
+++ App.js
@@ -1,112 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, { Component } from 'react';
+import { View, StyleSheet, ToastAndroid, Button ,Text,Image} from "react-native";
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+GoogleSignin.configure({
+  webClientId: '403824723231-81fko73qi2ambcvu4coruuk0gtt1nu0a.apps.googleusercontent.com',
+  offlineAccess: true, // if you want to access Google API on behalf 
 });
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      userGoogleInfo : {},
+      loaded: false
+    }
+
+  }
+  signIn = async () => {
+    try {
+      console.log("INICIO_LOGIN");
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log({ userInfo });
+      this.setState({
+        userGoogleInfo : userInfo,
+        loaded : true
+      })
+      console.log(this.state.userGoogleInfo);
+    } catch (error) {
+      console.log({ error });
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log("e 1");
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log("e 2");
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log("e 3");
+      } else {
+        console.log(error.message);
+      }
+    }
+  };
+  render() {
+    return (
+      <View>
+
+      <GoogleSigninButton
+          style={{ width: 222, height: 48 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={this.signIn}
+          />
+         {this.state.loaded ?
+          <View>
+            <Text>{this.state.userGoogleInfo.user.name}</Text>
+            <Text>{this.state.userGoogleInfo.user.email}</Text>
+            <Image 
+          style={{ width: 100, height: 100 }}
+          source={{uri: this.state.userGoogleInfo.user.photo}}
+        />
+
+          </View>
+        
+        : <Text>Not SignedIn</Text> }
+         
+        
+          </View>
+    );
+  }
+}
 
 export default App;
