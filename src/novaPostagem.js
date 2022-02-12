820ca9dcb0486e3147c4bb87188e8f71f4359dc7
Hugo Coelho <iProHugoo@outlook.com>
2022-02-12 15:12:49 -0300

    Ajustes na estilização básica de Nova Postagem

--- src/novaPostagem.js
+++ src/novaPostagem.js
@@ -3,51 +3,54 @@ import { View } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 
 export default class novaPostagem extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: ''
-        };
+    state = {
+        setTituloPostagem: {},
+        setDataEntrega: {},
+        setDescPostagem: {},
     }
-
-    selectDate = (date) => {
-        this.setState({ date: date });
-    }
-
 
     render() {
         return (
+            <View>
+                <View style={{ padding: 20 }}>
+                    <Text h4>Nova Postagem</Text>
+                    <Text label>Título da Postagem:</Text>
+                    <Input
+                        placeholder="Descrição da Postagem"
+                        style={{ fontSize: 16 }}
+                        onChangeText={value => {
+                            this.setState({ setTituloPostagem: value });
+                        }}
+                    />
+                    <Text label>Data de Entrega:</Text>
+                    <Input
+                        placeholder="Data de Entrega"
+                        style={{ fontSize: 16 }}
+                        onChangeText={value => {
+                            this.setState({ setDataEntrega: value });
+                        }}
+                    />
+                    <Text label>Descrição:</Text>
+                    <Input
+                        placeholder="Descrição da Postagem"
+                        style={{ fontSize: 16 }}
+                        onChangeText={value => {
+                            this.setState({ setDescPostagem: value });
+                        }}
+                    />
+                </View>
 
-            <View style={{ flex: 1, padding: 15, alignItems: "center" }}>
-
-                <Text h3>Nova Postagem</Text>
-
-                <Text label>Título da Postagem:</Text>
-
-                <Input
-                    placeholder=""
-                />
-
-                <Text label>Data de Entrega:</Text>
-
-                <Input
-                    placeholder="Selecione a Data de Entrega"
-                />
-
-                <Text label>Descrição:</Text>
-
-                <Input
-                    placeholder=""
-                />
-
-                <Button
-                    title="Salvar"
-                    onPress={() => criar()}
-                />
-
+                <View style={{ paddingHorizontal: 40, paddingHorizontal: 20 }}>
+                    <Button style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderRadius: 10,
+                    }}
+                        title="Salvar"
+                        onPress={() => criarPostagem()}
+                    />
+                </View>
             </View>
-
         );
     }
 }
